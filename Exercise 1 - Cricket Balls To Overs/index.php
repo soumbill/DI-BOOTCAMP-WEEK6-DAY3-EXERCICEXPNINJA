1993cf8b9e59a55b7a0bb98197a418ad09d214a0
soumbill <konesoumailabilly@gmail.com>
2023-02-02 20:06:50 +0100

    exoXPNINJA commit

---
+++ Exercise 1 - Cricket Balls To Overs/index.php
@@ -0,0 +1,12 @@
+<?php
+
+function totalOvers(int $balls): float | int  | string
+{
+    $overs = floor($balls / 6);
+
+    $ball = $balls % 6;
+
+    return (float) $overs .'.'. $ball;
+}
+
+echo totalOvers(5);
